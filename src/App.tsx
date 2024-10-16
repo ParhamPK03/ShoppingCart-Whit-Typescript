24e4bdf2cf29994e54905f668bec5da3e0e4f7be
Parham <parham.pourkhani23@gmail.com>
2024-10-16 09:01:24 +0330

    All

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import Header from "./Components/Header";
+import { useRoutes } from "react-router-dom";
+import routes from "./routes";
+function App() {
+  const router = useRoutes(routes);
+
+  return (
+    // <ContextDataProvider>
+    <div className="app">
+      <Header />
+      {/* Start Content */}
+
+      {router}
+
+      {/* Finish Content */}
+
+      <footer>
+        <a target={"_blank"} href="https://sabzlearn.ir">
+          Sabzlearn.ir
+        </a>
+      </footer>
+    </div>
+    // </ContextDataProvider>
+  );
+}
+
+export default App;
