3fbfa20363258c5fb3e26bef09a317ef1800895a
Parham <parham.pourkhani23@gmail.com>
2024-10-16 09:51:26 +0330

    C

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,27 @@ import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import Header from "./Components/Header";
 import { useRoutes } from "react-router-dom";
 import routes from "./routes";
+import CartContextProvider from "./context/cartContext";
 function App() {
   const router = useRoutes(routes);
 
   return (
-    // <ContextDataProvider>
-    <div className="app">
-      <Header />
-      {/* Start Content */}
+    <CartContextProvider>
+      <div className="app">
+        <Header />
+        {/* Start Content */}
 
-      {router}
+        {router}
 
-      {/* Finish Content */}
+        {/* Finish Content */}
 
-      <footer>
-        <a target={"_blank"} href="https://sabzlearn.ir">
-          Sabzlearn.ir
-        </a>
-      </footer>
-    </div>
-    // </ContextDataProvider>
+        <footer>
+          <a target={"_blank"} href="https://sabzlearn.ir">
+            Sabzlearn.ir
+          </a>
+        </footer>
+      </div>
+    </CartContextProvider>
   );
 }
 
--- src/context/cartContext.tsx
+++ src/context/cartContext.tsx
@@ -1,32 +1,49 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { Product } from "../Components/Products.types";
 
 type CartContextProviderProps = {
-    children: React.ReactNode
+  children: React.ReactNode;
 };
 
 type CartContextType = {
-    UserCart: Product[];
-    addProduct: (id: number) => void,
-    removeProduct: (id: number) => void,
-    removeAll: () => void,
-    shop: Product[];
+  UserCart: Product[];
+  addProduct: (id: number) => void;
+  removeProduct: (id: number) => void;
+  removeAll: () => void;
+  shop: Product[];
 };
 
-export const CartContext = createContext({} as CartContextType)
+export const CartContext = createContext({} as CartContextType);
 
 const CartContextProvider = ({ children }: CartContextProviderProps) => {
+  const [UserCart, setUserCart] = useState<Product[]>([]);
+  const [shop, setShop] = useState<Product[]>([]);
 
-    const [UserCart, setUserCart] = useState<Product[]>([])
-    const [shop, setShop] = useState<Product[]>([])
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setShop(data));
+  }, []);
 
-    const addProduct = (id: number) => { }
-    const removeProduct = (id: number) => { }
-    const removeAll = () => { }
+  const addProduct = (id: number) => {};
 
-    return (
-        <CartContextProvider value={{ addProduct, removeAll, removeProduct, shop, UserCart, }}>
-            {children}
-        </CartContextProvider>
-    )
-}
+  const removeProduct = (id: number) => {};
+
+  const removeAll = () => {};
+
+  return (
+    <CartContext.Provider
+      value={{
+        addProduct,
+        removeProduct,
+        removeAll,
+        shop,
+        UserCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartContextProvider;
